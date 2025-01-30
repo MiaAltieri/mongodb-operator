2141a9ab7b5ac27b60fe1fc2d7bc3edb028a7f4b
MiaAltieri <mgaltier200@gmail.com>
2025-01-30 16:16:32 +0000

    add test

--- tests/integration/sharding_tests/test_sharding_relations.py
+++ tests/integration/sharding_tests/test_sharding_relations.py
@@ -11,10 +11,12 @@ S3_APP_NAME = "s3-integrator"
 SHARD_ONE_APP_NAME = "shard"
 CONFIG_SERVER_ONE_APP_NAME = "config-server-one"
 CONFIG_SERVER_TWO_APP_NAME = "config-server-two"
+CERTS_APP_NAME = "self-signed-certificates"
 REPLICATION_APP_NAME = "replication"
 APP_CHARM_NAME = "application"
 MONGOS_APP_NAME = "mongos"
 MONGOS_HOST_APP_NAME = "application-host"
+CERT_REL_NAME = "certificates"
 
 SHARDING_COMPONENTS = [SHARD_ONE_APP_NAME, CONFIG_SERVER_ONE_APP_NAME]
 
@@ -56,6 +58,7 @@ async def test_build_and_deploy(
         MONGOS_APP_NAME,
         channel="6/edge",
     )
+    await ops_test.model.deploy(CERTS_APP_NAME, channel="stable")
     await ops_test.model.deploy(S3_APP_NAME, channel="edge")
 
     # TODO: Future PR, once data integrator works with mongos charm deploy that charm instead of
@@ -72,6 +75,7 @@ async def test_build_and_deploy(
             CONFIG_SERVER_ONE_APP_NAME,
             CONFIG_SERVER_TWO_APP_NAME,
             SHARD_ONE_APP_NAME,
+            CERTS_APP_NAME,
         ],
         idle_period=20,
         raise_on_blocked=False,
@@ -92,105 +96,226 @@ async def test_build_and_deploy(
     )
 
 
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_only_one_config_server_relation(ops_test: OpsTest) -> None:
+#     """Verify that a shard can only be related to one config server."""
+#     await ops_test.model.integrate(
+#         f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+#     with pytest.raises(JujuAPIError) as juju_error:
+#         await ops_test.model.integrate(
+#             f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
+#             f"{CONFIG_SERVER_TWO_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#         )
+
+#     assert (
+#         juju_error.value.args[0] == RELATION_LIMIT_MESSAGE
+#     ), "Shard can relate to multiple config servers."
+
+#     # clean up relation
+#     await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
+#         f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+#     await ops_test.model.wait_for_idle(
+#         apps=[REPLICATION_APP_NAME],
+#         idle_period=20,
+#         raise_on_blocked=False,
+#         timeout=TIMEOUT,
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_cannot_use_db_relation(ops_test: OpsTest) -> None:
+#     """Verify that sharding components cannot use the DB relation."""
+#     for sharded_component in SHARDING_COMPONENTS:
+#         await ops_test.model.integrate(
+#             f"{APP_CHARM_NAME}:{DATABASE_REL_NAME}", sharded_component
+#         )
+
+#     for sharded_component in SHARDING_COMPONENTS:
+#         await wait_for_mongodb_units_blocked(
+#             ops_test,
+#             sharded_component,
+#             status="Sharding roles do not support database interface.",
+#             timeout=300,
+#         )
+
+#     # clean up relations
+#     for sharded_component in SHARDING_COMPONENTS:
+#         await ops_test.model.applications[sharded_component].remove_relation(
+#             f"{APP_CHARM_NAME}:{DATABASE_REL_NAME}",
+#             sharded_component,
+#         )
+
+#     await ops_test.model.wait_for_idle(
+#         apps=SHARDING_COMPONENTS,
+#         idle_period=20,
+#         raise_on_blocked=False,
+#         timeout=TIMEOUT,
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_replication_config_server_relation(ops_test: OpsTest):
+#     """Verifies that using a replica as a shard fails."""
+#     # attempt to add a replication deployment as a shard to the config server.
+#     await ops_test.model.integrate(
+#         f"{REPLICATION_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+#     await wait_for_mongodb_units_blocked(
+#         ops_test,
+#         REPLICATION_APP_NAME,
+#         status="sharding interface cannot be used by replicas",
+#         timeout=300,
+#     )
+
+#     # clean up relations
+#     await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
+#         f"{REPLICATION_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_replication_shard_relation(ops_test: OpsTest):
+#     """Verifies that using a replica as a config-server fails."""
+#     # attempt to add a shard to a replication deployment as a config server.
+#     await ops_test.model.integrate(
+#         f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{REPLICATION_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+#     await wait_for_mongodb_units_blocked(
+#         ops_test,
+#         REPLICATION_APP_NAME,
+#         status="sharding interface cannot be used by replicas",
+#         timeout=300,
+#     )
+
+#     # clean up relation
+#     await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
+#         f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
+#         f"{REPLICATION_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+#     )
+
+#     await ops_test.model.wait_for_idle(
+#         apps=[REPLICATION_APP_NAME],
+#         idle_period=20,
+#         raise_on_blocked=False,
+#         timeout=TIMEOUT,
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_replication_mongos_relation(ops_test: OpsTest) -> None:
+#     """Verifies connecting a replica to a mongos router fails."""
+#     # attempt to add a replication deployment as a shard to the config server.
+#     await ops_test.model.integrate(
+#         f"{REPLICATION_APP_NAME}",
+#         f"{MONGOS_APP_NAME}",
+#     )
+
+#     await wait_for_mongodb_units_blocked(
+#         ops_test,
+#         REPLICATION_APP_NAME,
+#         status="Relation to mongos not supported, config role must be config-server",
+#         timeout=300,
+#     )
+
+#     # clean up relations
+#     await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
+#         f"{REPLICATION_APP_NAME}:cluster",
+#         f"{MONGOS_APP_NAME}:cluster",
+#     )
+
+#     await ops_test.model.wait_for_idle(
+#         apps=[SHARD_ONE_APP_NAME],
+#         idle_period=20,
+#         raise_on_blocked=False,
+#         timeout=TIMEOUT,
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_shard_mongos_relation(ops_test: OpsTest) -> None:
+#     """Verifies connecting a shard to a mongos router fails."""
+#     # attempt to add a replication deployment as a shard to the config server.
+#     await ops_test.model.integrate(
+#         f"{SHARD_ONE_APP_NAME}",
+#         f"{MONGOS_APP_NAME}",
+#     )
+
+#     await wait_for_mongodb_units_blocked(
+#         ops_test,
+#         SHARD_ONE_APP_NAME,
+#         status="Relation to mongos not supported, config role must be config-server",
+#         timeout=300,
+#     )
+
+#     # clean up relations
+#     await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
+#         f"{MONGOS_APP_NAME}:cluster",
+#         f"{SHARD_ONE_APP_NAME}:cluster",
+#     )
+
+
+# @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
+# @pytest.mark.group(1)
+# @pytest.mark.abort_on_fail
+# async def test_shard_s3_relation(ops_test: OpsTest) -> None:
+#     """Verifies integrating a shard to s3-integrator fails."""
+#     # attempt to add a replication deployment as a shard to the config server.
+#     await ops_test.model.integrate(
+#         f"{SHARD_ONE_APP_NAME}",
+#         f"{S3_APP_NAME}",
+#     )
+
+#     await wait_for_mongodb_units_blocked(
+#         ops_test,
+#         SHARD_ONE_APP_NAME,
+#         status="Relation to s3-integrator is not supported, config role must be config-server",
+#         timeout=300,
+#     )
+
+#     # clean up relations
+#     await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
+#         f"{S3_APP_NAME}:s3-credentials",
+#         f"{SHARD_ONE_APP_NAME}:s3-credentials",
+#     )
+
+
 @pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
 @pytest.mark.group(1)
 @pytest.mark.abort_on_fail
-async def test_only_one_config_server_relation(ops_test: OpsTest) -> None:
-    """Verify that a shard can only be related to one config server."""
-    await ops_test.model.integrate(
-        f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
-        f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
-    )
-
-    with pytest.raises(JujuAPIError) as juju_error:
-        await ops_test.model.integrate(
-            f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
-            f"{CONFIG_SERVER_TWO_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
-        )
-
-    assert (
-        juju_error.value.args[0] == RELATION_LIMIT_MESSAGE
-    ), "Shard can relate to multiple config servers."
-
-    # clean up relation
-    await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
-        f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
-        f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
-    )
-
-    await ops_test.model.wait_for_idle(
-        apps=[REPLICATION_APP_NAME],
-        idle_period=20,
-        raise_on_blocked=False,
-        timeout=TIMEOUT,
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_cannot_use_db_relation(ops_test: OpsTest) -> None:
-    """Verify that sharding components cannot use the DB relation."""
-    for sharded_component in SHARDING_COMPONENTS:
-        await ops_test.model.integrate(f"{APP_CHARM_NAME}:{DATABASE_REL_NAME}", sharded_component)
-
-    for sharded_component in SHARDING_COMPONENTS:
-        await wait_for_mongodb_units_blocked(
-            ops_test,
-            sharded_component,
-            status="Sharding roles do not support database interface.",
-            timeout=300,
-        )
-
-    # clean up relations
-    for sharded_component in SHARDING_COMPONENTS:
-        await ops_test.model.applications[sharded_component].remove_relation(
-            f"{APP_CHARM_NAME}:{DATABASE_REL_NAME}",
-            sharded_component,
-        )
-
-    await ops_test.model.wait_for_idle(
-        apps=SHARDING_COMPONENTS,
-        idle_period=20,
-        raise_on_blocked=False,
-        timeout=TIMEOUT,
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_replication_config_server_relation(ops_test: OpsTest):
-    """Verifies that using a replica as a shard fails."""
-    # attempt to add a replication deployment as a shard to the config server.
-    await ops_test.model.integrate(
-        f"{REPLICATION_APP_NAME}:{SHARD_REL_NAME}",
-        f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
-    )
-
-    await wait_for_mongodb_units_blocked(
-        ops_test,
-        REPLICATION_APP_NAME,
-        status="sharding interface cannot be used by replicas",
-        timeout=300,
-    )
-
-    # clean up relations
-    await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
-        f"{REPLICATION_APP_NAME}:{SHARD_REL_NAME}",
-        f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_replication_shard_relation(ops_test: OpsTest):
-    """Verifies that using a replica as a config-server fails."""
+async def test_config_server_tls_replication_relation(ops_test: OpsTest) -> None:
+    """Verifies that using a replica as a shard fails even when TLS is integrated."""
     # attempt to add a shard to a replication deployment as a config server.
     await ops_test.model.integrate(
-        f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
-        f"{REPLICATION_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
+        f"{REPLICATION_APP_NAME}",
+        f"{CERTS_APP_NAME}",
+    )
+
+    await ops_test.model.integrate(
+        f"{REPLICATION_APP_NAME}:{SHARD_REL_NAME}",
+        f"{CONFIG_SERVER_ONE_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
     )
 
     await wait_for_mongodb_units_blocked(
@@ -200,7 +325,12 @@ async def test_replication_shard_relation(ops_test: OpsTest):
         timeout=300,
     )
 
-    # clean up relation
+    # clean up relations
+    await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
+        f"{CERTS_APP_NAME}:{CERT_REL_NAME}",
+        f"{REPLICATION_APP_NAME}:{CERT_REL_NAME}",
+    )
+
     await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
         f"{SHARD_ONE_APP_NAME}:{SHARD_REL_NAME}",
         f"{REPLICATION_APP_NAME}:{CONFIG_SERVER_REL_NAME}",
@@ -211,86 +341,4 @@ async def test_replication_shard_relation(ops_test: OpsTest):
         idle_period=20,
         raise_on_blocked=False,
         timeout=TIMEOUT,
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_replication_mongos_relation(ops_test: OpsTest) -> None:
-    """Verifies connecting a replica to a mongos router fails."""
-    # attempt to add a replication deployment as a shard to the config server.
-    await ops_test.model.integrate(
-        f"{REPLICATION_APP_NAME}",
-        f"{MONGOS_APP_NAME}",
-    )
-
-    await wait_for_mongodb_units_blocked(
-        ops_test,
-        REPLICATION_APP_NAME,
-        status="Relation to mongos not supported, config role must be config-server",
-        timeout=300,
-    )
-
-    # clean up relations
-    await ops_test.model.applications[REPLICATION_APP_NAME].remove_relation(
-        f"{REPLICATION_APP_NAME}:cluster",
-        f"{MONGOS_APP_NAME}:cluster",
-    )
-
-    await ops_test.model.wait_for_idle(
-        apps=[SHARD_ONE_APP_NAME],
-        idle_period=20,
-        raise_on_blocked=False,
-        timeout=TIMEOUT,
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_shard_mongos_relation(ops_test: OpsTest) -> None:
-    """Verifies connecting a shard to a mongos router fails."""
-    # attempt to add a replication deployment as a shard to the config server.
-    await ops_test.model.integrate(
-        f"{SHARD_ONE_APP_NAME}",
-        f"{MONGOS_APP_NAME}",
-    )
-
-    await wait_for_mongodb_units_blocked(
-        ops_test,
-        SHARD_ONE_APP_NAME,
-        status="Relation to mongos not supported, config role must be config-server",
-        timeout=300,
-    )
-
-    # clean up relations
-    await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
-        f"{MONGOS_APP_NAME}:cluster",
-        f"{SHARD_ONE_APP_NAME}:cluster",
-    )
-
-
-@pytest.mark.runner(["self-hosted", "linux", "X64", "jammy", "large"])
-@pytest.mark.group(1)
-@pytest.mark.abort_on_fail
-async def test_shard_s3_relation(ops_test: OpsTest) -> None:
-    """Verifies integrating a shard to s3-integrator fails."""
-    # attempt to add a replication deployment as a shard to the config server.
-    await ops_test.model.integrate(
-        f"{SHARD_ONE_APP_NAME}",
-        f"{S3_APP_NAME}",
-    )
-
-    await wait_for_mongodb_units_blocked(
-        ops_test,
-        SHARD_ONE_APP_NAME,
-        status="Relation to s3-integrator is not supported, config role must be config-server",
-        timeout=300,
-    )
-
-    # clean up relations
-    await ops_test.model.applications[SHARD_ONE_APP_NAME].remove_relation(
-        f"{S3_APP_NAME}:s3-credentials",
-        f"{SHARD_ONE_APP_NAME}:s3-credentials",
     )
